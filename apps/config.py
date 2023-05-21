cd75d0231b19670a11a40c744b5bf37425ab815c
tmeralus <tmeralus@gmail.com>
2023-05-21 10:40:17 -0400

    updated env vars

--- apps/config.py
+++ apps/config.py
@@ -2,26 +2,18 @@
 """
 Copyright (c) 2023 - present Tedley Meralus
 """
-
 import os
 import psycopg2
+from dotenv import load_dotenv
 from decouple import config
 
 class Config(object):
-
+    load_dotenv() # load .env variables 
     basedir = os.path.abspath(os.path.dirname(__file__))
 
-    # Set up the App SECRET_KEY
-    SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
-
-    # This will connect to the postgres db container service named 'db'
-    # postgresql://scott:tiger@localhost/mydatabase 
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@db/postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # This will create a file in <app> FOLDER
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Set up the App keys  
+    SECRET_KEY = os.getenv('POSTGRES_USER')
+    SECRET_KEY = os.getenv('POSTGRES_PASSWORD') 
 
 
 class ProductionConfig(Config):
@@ -32,8 +24,9 @@ class ProductionConfig(Config):
     REMEMBER_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_DURATION = 3600
 
-    # PostgreSQL database
-    SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
+    # This will connect to the postgres db container service named 'db' 
+    # postgresql+psycopg2://{db_login}:{db_password}@db:5432/postgres
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/postgres'.format(
         config('DB_ENGINE', default='postgresql'),
         config('DB_USERNAME', default='postgres'),
         config('DB_PASS', default='postgres'),
